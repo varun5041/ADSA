f829ad40e4bb75bf1e4ec793b814d09826b820c3
VARUN SONI <varunsoni21102005@gmail.com>
2025-07-30 09:40:35 +0530

    BST.C

---
+++ BST.C
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define Node structure
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create a new node
+struct Node* createNode(int value) {
+    struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+    newnode->data = value;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+// Insert value into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Inorder traversal: Left -> Root -> Right
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+// Preorder traversal: Root -> Left -> Right
+void preorder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Postorder traversal: Left -> Right -> Root
+void postorder(struct Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+// Main program with menu
+int main() {
+    struct Node* root = NULL;
+    int choice, value;
+
+    printf("=== Binary Search Tree Implementation ===\n");
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Insert value\n");
+        printf("2. Inorder traversal\n");
+        printf("3. Preorder traversal\n");
+        printf("4. Postorder traversal\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                printf("Inserted %d successfully.\n", value);
+                break;
+            case 2:
+                printf("Inorder traversal: ");
+                inorder(root);
+                printf("\n");
+                break;
+            case 3:
+                printf("Preorder traversal: ");
+                preorder(root);
+                printf("\n");
+                break;
+            case 4:
+                printf("Postorder traversal: ");
+                postorder(root);
+                printf("\n");
+                break;
+            case 5:
+                printf("Exiting program.\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    return 0;
+}
